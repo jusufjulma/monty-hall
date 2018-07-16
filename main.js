7e43c4a09fe0b1d39b47158cee087772e1a473c2
jusufjulma <jusaleander@hotmail.com>
2018-07-17 02:34:59 +0300

    added functionality, edited appearance

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let doors = [0, 0, 0];
 let doorsId = ["#one", "#two", "#three"];
 let pick = [0, 0, 0];
+let hintNeeded = 1;
 
 
 function main() {
@@ -11,12 +12,20 @@ function main() {
   // judgement();
 }
 
+let hint = () =>{       //  if user is stuck on the beginning
+  if(hintNeeded == 1){  // well, if not
+  $(".advice-click-door").css({"display": "block"});
+  }
+}
+
 function doorWin() {  // here the winning door is decided
   doors = [0, 0, 0];
   pick = [0, 0, 0];
   let winning = Math.floor(Math.random()*3);  // "randomly" of course
   doors[winning] = 1;                         // value 1 given to correct door
   $(".grid-cell").css({"background-color": "#651717"});  // reset colors
+
+  setTimeout (hint, 20000);     // waiting for user to read  the isntructions
   // colors(winning);  // should not be on final version
 } // end of function
 
@@ -43,11 +52,15 @@ function colors(winning) {
 } // end of fucntion
 
 function userPick(door) { // here user pics door
+  hintNeeded = 0;
   pick = [0, 0, 0];
-  pick[door-1] = 1;       // which is then logged to pick-array
+  let selectedDoor = door-1;    // this was necessary to get selection coloring
+  pick[selectedDoor] = 1;       // which is then logged to pick-array
+  $(doorsId[selectedDoor]).css({"box-shadow": "0 0 2rem 0.5rem rgba(200, 40, 0, 0.8)"})
   setTimeout(revealWrong, 500);           // executing next function
   console.log("What the fuck");
   setTimeout(bringQuestion, 2000)
+  $(".advice-click-door").css({"display": "none"});
 
 }
 
@@ -65,15 +78,18 @@ function revealWrong() {  // this opens ONE WRONG door on random
     if (doors[i] == 0 && pick[i] == 0) {  // if door is "lose",
       switch (i) {                        // paint that door grey
         case 0:                           // unless user picked it (&&)
-          $("#one").css({"background-color": "#111", "color": "rgba(0, 0, 0, 0)", "text-shadow": "none"});
+          $("#one").css({"background-image": "url(images/goat.jpg)",
+          "color": "rgba(0, 0, 0, 0)", "text-shadow": "none"});
           doors[i] = 2; pick[i] = 2;
           break;
         case 1:
-          $("#two").css({"background-color": "#111", "color": "rgba(0, 0, 0, 0)", "text-shadow": "none"});
+          $("#two").css({"background-image": "url(images/goat.jpg)",
+          "color": "rgba(0, 0, 0, 0)", "text-shadow": "none"});
           doors[i] = 2; pick[i] = 2;
           break;
         case 2:
-          $("#three").css({"background-color": "#111", "color": "rgba(0, 0, 0, 0)", "text-shadow": "none"});
+          $("#three").css({"background-image": "url(images/goat.jpg)",
+          "color": "rgba(0, 0, 0, 0)", "text-shadow": "none"});
           doors[i] = 2; pick[i] = 2;
           break;
         default:
@@ -97,8 +113,12 @@ function userSwitch(decision) {
       break;
     case "y":
     console.log("Switched!");
+    $(doorsId[0]).css({"box-shadow": "none"});
+    $(doorsId[1]).css({"box-shadow": "none"});
+    $(doorsId[2]).css({"box-shadow": "none"});
     for (i = 0; i < pick.length; i++){
-      if (pick[i] == 0){pick[i] = 1
+      if (pick[i] == 0){pick[i] = 1;
+        $(doorsId[i]).css({"box-shadow": "0 0 2rem 0.5rem rgba(200, 40, 0, 0.8)"})
       }else if (pick[i] == 1) {pick[i] = 0
       }else{
         continue;
@@ -119,11 +139,11 @@ function judgement() {
   } console.log(finalPick); // end of for
   if (doors[finalPick] == 1){
     console.log("Winner!");
-    $(doorsId[finalPick]).css({"background-image": "url(images/win.png)",
+    $(doorsId[finalPick]).css({"background-image": "url(images/car.jpeg)",
     "color": "rgba(0, 0, 0, 0)", "text-shadow": "none"});
   }else{
     console.log("Loser.");
-    $(doorsId[finalPick]).css({"background-image": "url(images/lose.png)",
+    $(doorsId[finalPick]).css({"background-image": "url(images/goat.jpg)",
     "color": "rgba(0, 0, 0, 0)", "text-shadow": "none"});
 
   }
